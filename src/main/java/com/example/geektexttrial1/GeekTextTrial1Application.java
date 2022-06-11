67cb338ba4e919711796b7c0dfddc64eb43e5012
Knockout726 <koliv054@fiu.edu>
2022-06-11 11:01:38 -0400

    Initial commit

---
+++ src/main/java/com/example/geektexttrial1/GeekTextTrial1Application.java
@@ -0,0 +1,14 @@
+package com.example.geektexttrial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTextTrial1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekTextTrial1Application.class, args);
+    }
+
+    
+}
